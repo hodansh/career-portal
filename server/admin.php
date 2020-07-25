03ba2cc1d9b6783fd39279dc05adf2484485f231
sfratt <sfratt@users.noreply.github.com>
2020-07-24 21:45:35 -0400

    Add Docker and PHP files to workspace

---
+++ server/admin.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>ADMIN PAGE</header>
+    <div class="data">
+        <h1> THIS PROJECT SUCKS! </h1>
+
+        <form id="foo" method="POST">
+            <br>
+            <label>Insert Query</label>
+            <br>
+            <textarea name="query10" rows="5" cols="50">
+                <?php if (isset($_POST['query10'])) {
+                                                            echo htmlentities($_POST['query10']);
+                                                        } ?></textarea>
+            <br>
+            <input type="submit" value="Send Query" name="anyQuery" />
+        </form>
+        <div id="commandLine">
+            <?php
+            $servername = "127.0.0.1:12000";
+            $username = "qyc353_1";
+            $password = "1234qwer";
+            $dbname = "qyc353_1";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+
+            if (isset($_POST['anyQuery'])) {
+                $sql = $_POST['query10'];
+                $insert = "insert";
+                $delete = "delete";
+                if (stripos($sql, $insert) !== FALSE) {
+                    $result = $conn->query($sql) or die($conn->error);
+                    echo "Item added";
+                } elseif (stripos($sql, $delete) !== FALSE) {
+                    $result = $conn->query($sql) or die($conn->error);
+                    echo "Item deleted";
+                } else {
+                    $result = $conn->query($sql) or die($conn->error);
+                    while ($row = $result->fetch_assoc()) {
+                        print("<pre>" . print_r($row, true) . "</pre>");
+                    }
+                }
+            }
+            $conn->close();
+            ?>
+        </div>
+
+    </div>
+</body>
+
+</html>
