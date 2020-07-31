132d547bd1502aac813163822160fa03004299b6
Hicham <hicham_daou@hotmail.com>
2020-07-30 23:20:02 -0400

    turned all statuses into smallint, asserted not null and unique fields

--- database/create_db.sql
+++ database/create_db.sql
@@ -5,8 +5,8 @@ USE CareerPortal;
 CREATE TABLE EmployeeCategory
 (
     EmployeeCategoryId INT NOT NULL AUTO_INCREMENT,
-    Status VARCHAR(100),
-    MonthlyCharge VARCHAR(100),
+    Status SMALLINT NOT NULL,
+    MonthlyCharge VARCHAR(100) NOT NULL,
     MaxJobs VARCHAR(100),
     PRIMARY KEY (EmployeeCategoryId)
 );
@@ -14,8 +14,8 @@ CREATE TABLE EmployeeCategory
 CREATE TABLE EmployerCategory
 (
     EmployerCategoryId INT NOT NULL AUTO_INCREMENT,
-    Status VARCHAR(100),
-    MonthlyCharge VARCHAR(100),
+    Status SMALLINT NOT NULL,
+    MonthlyCharge VARCHAR(100) NOT NULL,
     MaxJobs VARCHAR(100),
     PRIMARY KEY (EmployerCategoryId)
 );
@@ -23,14 +23,14 @@ CREATE TABLE EmployerCategory
 CREATE TABLE Employer
 (
 	EmployerId INT NOT NULL AUTO_INCREMENT,
-    UserName VARCHAR(100),
-    UserPassword VARCHAR(100),
+    UserName VARCHAR(100) UNIQUE NOT NULL,
+    UserPassword VARCHAR(100) NOT NULL,
     Email VARCHAR(100),
-    Company VARCHAR(100),
-    Telephone VARCHAR(12),
-    PostalCode VARCHAR(6),
-    City VARCHAR(100),
-    Address VARCHAR(100),
+    Company VARCHAR(100) UNIQUE NOT NULL,
+    Telephone VARCHAR(12) NOT NULL,
+    PostalCode VARCHAR(6) NOT NULL,
+    City VARCHAR(100) NOT NULL,
+    Address VARCHAR(100) NOT NULL,
     EmployerCategoryId INT NOT NULL,
     PRIMARY KEY (EmployerId),
     FOREIGN KEY (EmployerCategoryId) REFERENCES EmployerCategory (EmployerCategoryId)
@@ -39,10 +39,10 @@ CREATE TABLE Employer
 CREATE TABLE Job
 (
     JobId INT NOT NULL AUTO_INCREMENT,
-    Title VARCHAR(100),
-    Category VARCHAR(100),
-    JobDescription VARCHAR(100),
-    DatePosted DATE,
+    Title VARCHAR(100) NOT NULL,
+    Category SMALLINT NOT NULL,
+    JobDescription VARCHAR(100) NOT NULL,
+    DatePosted DATE NOT NULL,
     NeededEmployees INT NOT NULL,
     AppliedEmployees INT NOT NULL,
     AcceptedOffers INT NOT NULL,
@@ -54,14 +54,13 @@ CREATE TABLE Job
 CREATE TABLE Employee
 (
 	EmployeeId INT NOT NULL AUTO_INCREMENT,
-    UserName VARCHAR(100),
-    UserPassword VARCHAR(100),
-    Email VARCHAR(100),
-    Company VARCHAR(100),
-    Telephone VARCHAR(12),
-    PostalCode VARCHAR(6),
-    City VARCHAR(100),
-    Address VARCHAR(100),
+    UserName VARCHAR(100) UNIQUE NOT NULL,
+    UserPassword VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    Telephone VARCHAR(12) NOT NULL,
+    PostalCode VARCHAR(6) NOT NULL,
+    City VARCHAR(100) NOT NULL,
+    Address VARCHAR(100) NOT NULL,
     EmployeeCategoryId INT NOT NULL,
     PRIMARY KEY (EmployeeId),
     FOREIGN KEY (EmployeeCategoryId) REFERENCES EmployeeCategory (EmployeeCategoryId)
@@ -71,7 +70,7 @@ CREATE TABLE JobApplication
 (
     EmployeeId INT NOT NULL,
     JobId INT NOT NULL,
-    Status VARCHAR(100),
+    Status SMALLINT NOT NULL,
     PRIMARY KEY (EmployeeId, JobId),
     FOREIGN KEY (EmployeeId) REFERENCES Employee (EmployeeId),
     FOREIGN KEY (JobId) REFERENCES Job (JobId)
@@ -81,7 +80,7 @@ CREATE TABLE JobOffer
 (
     EmployeeId INT NOT NULL,
     JobId INT NOT NULL,
-    Status VARCHAR(100),
+    Status SMALLINT NOT NULL,
     CreationDate date NOT NULL,
     PRIMARY KEY (EmployeeId, JobId),
     FOREIGN KEY (EmployeeId) REFERENCES Employee (EmployeeId),
