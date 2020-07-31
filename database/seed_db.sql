57845d086d015617839f154c1931f7f7eedb64fa
deeppatel-11 <deeppatel770@gmail.com>
2020-07-30 23:51:52 -0400

    Created seed_db and added some employer tuples

--- database/seed_db.sql
+++ database/seed_db.sql
@@ -1,5 +1,23 @@
-INSERT INTO UserCategory
-    (UserCategoryId, Status, MonthlyCharge, MaxJobs, IsForEmployer)
+INSERT INTO Job
+    (JobId, Title, Category, JobDescription, DatePosted, NeededEmployees, AppliedEmployees, AcceptedOffers, EmployerId)
 VALUES
-    ("2151 Et Rd.,Pont-Saint-Martin,LB26 6AY,Greenland");
+    ;
+    
+INSERT INTO Employer
+    (UserName, UserPassword, Email, Company, Telephone, PostalCode, City, Address, EmployerCategoryId)
+VALUES ("cARIdelS","PIN70ICY1AX","est@adui.net","Tempor Corporation","(607) 973-2300","816196","Dera Ismail Khan","Ap #812-3381 Ligula. Av." , 1),
+("EDBuQuIp", "DEL33VHC5PT","Mauris.eu.turpis@sed.org","Dictum Cursus Nunc Industries","(787) 607-5124","423001","Heestert","467-7255 Lectus Street" , 2),
+("RKowNtAt", "VVC00ZXW5RI","dictum.eleifend.nunc@Nullasemper.org","In Tempus Eu Industries","(492) 421-6534","65107","Holyhead","3488 Montes, Av.", 3),
+("TOcHraNT", "TUP89PFG4DD","lacus@cursus.edu","Quis Institute","(600) 516-5717","52691","Rengo","Ap #244-7326 Odio St." , 4),
+("ICANTERt", "OXU11KYH7TB","Cum.sociis@iderat.com","Ipsum Leo Inc.","(545) 952-7187","HW25 7CR","Hillsboro","9356 Orci. Rd." , 5 ),
+("rLICkErD", "YUJ17TMY9RP","urna.Vivamus@Integer.net","Lorem Corp.","(958) 640-4747","23427","Hermosillo","761 Non, Avenue", 6),
+("tEgRamen", "FRD46PUN3IR","Donec.porttitor@eratin.ca","Sed Institute","(200) 498-5397","39490","Maria","P.O. Box 101, 6691 Phasellus Street", 7),
+("ORNMoStA", "GAB42XZN6IP","est@ametrisusDonec.co.uk","Est Nunc Inc.","(258) 602-0597","51077","Warisoulx","P.O. Box 529, 9904 Dolor, Ave" , 8);
+
+INSERT INTO Employee
+    (EmployeeId, UserName, UserPassword, Email, Telephone, PostalCode, City, Address, EmployeeCategoryId)
+VALUES
+    ;
+
+
 
